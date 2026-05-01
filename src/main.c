ee26267ec072b5092581dd39e0fd499e5c652ae8
noterdamepari <noterdamepari@icloud.com>
2026-05-01 21:24:17 +0700

    header generating and hashing

--- src/main.c
+++ src/main.c
@@ -33,17 +33,43 @@ char meow_init(){
 }
 
 void meow_add(char* file){
-    char buf[255];
+    char path[255];
     if (!is_path_absolute(file)) {
-        getcwd(buf, 100);
-        strcat(buf, "/");
-        strcat(buf, file);
+        getcwd(path, 100);
+        strcat(path, "/");
+        strcat(path, file);
+    } else {
+        strcpy(path, file);
     }
-    printf("[dbg] %s", buf);
+
+    struct stat st;
+    if (stat(path, &st) != 0) return;
+
+    SHA1_CTX sha;
+    SHA1Init(&sha);
+
+    char header[64];
+    int h_len = snprintf(header, sizeof(header), "blob %ld", st.st_size);
+    h_len++;
+
+    printf("%s\n", header);
+
+    uint8_t results[CHUNK];
+
+    FILE* f = fopen(path, "rb");
+
+    SHA1Update(&sha, (uint8_t *)header, h_len); // хешируем хедер блоба
+
+    SHA1Final(results, &sha);
+
+    printf("0x"); 
+    for (int i = 0; i < 20; i++) printf("%02x", results[i]);
+
+    putchar('\n');
+    fclose(f);
 }
 
 int main(int argc, char** argv){
-
     switch (argc){
         case 2:{
             if (!strcmp(argv[1], "init")) meow_init();
