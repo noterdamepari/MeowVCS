f9ef087fddde68314c27772a3a97eb17f5417fdc
noterdamepari <noterdamepari@icloud.com>
2026-05-01 17:38:51 +0700

    folder creating

--- include/meow.h
+++ include/meow.h
@@ -1,3 +1,18 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#ifndef MEOW_H
+#define MEOW_H
+
+    #include <stdio.h>
+    #include <string.h>
+    #include <stdlib.h>
+
+    #ifdef _WIN32
+        #include <direct.h>
+        #define mkdir(path, mode) _mkdir(path)
+        #define getcwd(buf, size) _getcwd(buf, size)
+    #else
+        #include <sys/stat.h>
+        #include <sys/types.h>
+        #include <unistd.h>
+    #endif
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,8 +1,20 @@
 #include "meow.h"
 
 int main(int argc, char** argv){
-    if (argc == 1){
+    const char* default_dir = "/.meow";
+    switch (argc){
+    case 2:{
+        if (!strcmp(argv[1], "init")){
+            char buf[100];
+            getcwd(buf, 100);
+            strcat(buf, default_dir);
+            mkdir(buf, 0777);
+        }
+        break;
+    } 
+    default:
         puts("Err: No arguments");
+        break;
     }
     return 0;
 }
