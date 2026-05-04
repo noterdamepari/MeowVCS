6498536da0a4941cb7672cee74e8d081b72b3958
noterdamepari <noterdamepari@icloud.com>
2026-05-04 20:52:42 +0700

    meow_add completed

--- include/meow.h
+++ include/meow.h
@@ -17,10 +17,16 @@
     #define CHUNK 16384 // 16kb
     #define PATH_MAX 4096
 
+
+    extern const char* default_dir;
+    extern const char* objects_dir;
+    extern const char* meow_index;
+
     char is_path_absolute(char* path);
     char find_work_dir(char* buffer);
     void find_project_dir(char* buffer, char* work_dir);
-    char add_to_indexfile(char* path, char* hash, char* work_dir);
+    int make_path_relative(const char *root, const char *input, char *output);
+    char create_object(char* path, char* work_dir, struct stat* st, char* hash);
     void get_object_path(char *dest, const char *work_dir, uint8_t *hash);
     int def(FILE *source, FILE *dest, int level);
     int inf(FILE *source, FILE *dest);
