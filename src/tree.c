1a0a8d04ad59efa4d76708aab460822875eab924
noterdamepari <noterdamepari@icloud.com>
2026-05-09 13:57:04 +0700

    dir_traverse improves

--- src/tree.c
+++ src/tree.c
@@ -1,5 +1,47 @@
 #include "meow.h"
 
+static void dir_traverse(const char* current_dir, const char* project_dir){
+    DIR* dir;
+    struct dirent* ent;
+    struct stat st;
+
+    dir = opendir(current_dir);
+
+    if (dir == NULL) assert(5);
+
+    while((ent=readdir(dir)) != 0){
+        if (!strcmp(ent->d_name, ".") || !strcmp(ent->d_name, "..") || !strcmp(ent->d_name, ".meow"))
+            continue;
+        char path[PATH_MAX];
+        snprintf(path, PATH_MAX, "%s/%s", current_dir, ent->d_name);
+        stat(path, &st);
+        if(S_ISDIR(st.st_mode)){
+            printf("\n[%s]\n", ent->d_name);
+            dir_traverse(path, project_dir);
+            printf("\n");
+        }
+    }
+
+    rewinddir(dir);
+
+
+    while((ent=readdir(dir)) != 0){
+        if (!strcmp(ent->d_name, ".") || !strcmp(ent->d_name, "..") || !strcmp(ent->d_name, ".meow"))
+            continue;
+        char path[PATH_MAX];
+        snprintf(path, PATH_MAX, "%s/%s", current_dir, ent->d_name);
+        stat(path, &st);
+        if(!S_ISDIR(st.st_mode)){
+            char rel_path[PATH_MAX];
+            make_path_relative(project_dir, path, rel_path);
+            printf("%s\n", rel_path);
+        }
+    }
+
+    closedir(dir);
+}
+
+
 void write_project_dir(){ 
     char work_dir[PATH_MAX];
     char project_dir[PATH_MAX];
@@ -10,31 +52,5 @@ void write_project_dir(){
     }
     find_project_dir(project_dir, work_dir);
 
-    dir_traverse(project_dir);
-}
-
-void dir_traverse(const char* project_dir){
-    DIR* dir;
-    struct dirent* ent;
-    struct stat st;
-
-    dir = opendir(project_dir);
-
-    if (dir == NULL) assert(5);
-
-    while((ent=readdir(dir)) != 0){
-        if (strcmp(ent->d_name, ".") && strcmp(ent->d_name, "..") && strcmp(ent->d_name, ".meow")){
-            char path[PATH_MAX];
-            snprintf(path, PATH_MAX, "%s/%s", project_dir, ent->d_name);
-            stat(path, &st);
-            if(S_ISDIR(st.st_mode)){
-                printf("[%s]\n", ent->d_name);
-                dir_traverse(path);
-            } else { 
-                printf("%s\n", ent->d_name);
-            }
-        }
-
-    }
-    closedir(dir);
+    dir_traverse(project_dir, project_dir);
 }
