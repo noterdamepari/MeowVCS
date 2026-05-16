acf8df9d8459ff4ee7d0360a0884ceced28c277e
noterdamepari <noterdamepari@icloud.com>
2026-05-16 16:21:46 +0700

    commit work started

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
 #include "meow.h"
+#include <stdio.h>
 
 const char* default_dir = "/.meow";
 const char* objects_dir = "/objects";
 const char* meow_index = "/index";
 
-char meow_init(){
+char meow_init() {
     char def_path[PATH_MAX];
     char path[PATH_MAX];
 
@@ -16,25 +17,26 @@ char meow_init(){
     strcpy(path, def_path);
     strcat(path, meow_index);
     FILE* index = fopen(path, "wb");
-    if (!index) perror("Cannot create index");
+    if (!index)
+        perror("Cannot create index");
     fprintf(index, "0");
     fclose(index);
 
     strcpy(path, def_path);
     strcat(path, objects_dir);
-    mkdir(path, 0777); 
+    mkdir(path, 0777);
     return 0;
 }
 
-void meow_add(char* file){
+void meow_add(char* file) {
     char work_dir[PATH_MAX];
     char path_to_index[PATH_MAX];
     char path_to_indextmp[PATH_MAX];
     char path[PATH_MAX];
 
-    if(find_work_dir(work_dir) == -1){ 
+    if (find_work_dir(work_dir) == -1) {
         fprintf(stderr, "Error: .meow/ not found\n");
-        return; 
+        return;
     }
 
     if (!is_path_absolute(file)) {
@@ -59,11 +61,12 @@ void meow_add(char* file){
     FILE* index = fopen(path_to_index, "rb");
     if (!index) {
         fprintf(stderr, "Error: index not found\n");
-        return; 
+        return;
     }
 
     FILE* index_tmp = fopen(path_to_indextmp, "wb");
-    if (!index) perror("index.tmp not created");
+    if (!index)
+        perror("index.tmp not created");
 
     fprintf(index_tmp, "0\n");
     unsigned int entries_amt;
@@ -76,17 +79,17 @@ void meow_add(char* file){
     find_project_dir(project_dir, work_dir);
     make_path_relative(project_dir, path, rel_path);
 
-    char tmpbuffer[PATH_MAX+64];
+    char tmpbuffer[PATH_MAX + 64];
 
     char inserted = 0;
     int new_entries_amt = entries_amt;
-    
-    for (int i = 0; i < entries_amt; i++){
+
+    for (int i = 0; i < entries_amt; i++) {
         fscanf(index, "%40s %hhu %lld %s", entry.hash, &entry.status, &entry.mtime, entry.path);
         char strcmp_res = strcmp(rel_path, entry.path);
-        if (!strcmp_res){ // already in index -> modified
+        if (!strcmp_res) { // already in index -> modified
             inserted = 1;
-            if (entry.mtime != file_mtime){ // nothing to do
+            if (entry.mtime != file_mtime) { // nothing to do
                 uint8_t hex_hash[41];
                 create_blob(path, work_dir, &st, hex_hash);
                 entry.status = 0;
@@ -100,7 +103,7 @@ void meow_add(char* file){
                 return;
             }
             fprintf(index_tmp, "%s %hhu %lld %s\n", entry.hash, entry.status, entry.mtime, entry.path);
-        } else if (strcmp_res < 0 && !inserted){
+        } else if (strcmp_res < 0 && !inserted) {
             inserted = 1;
             new_entries_amt++;
 
@@ -112,7 +115,7 @@ void meow_add(char* file){
             new_entry.status = 1;
             new_entry.mtime = file_mtime;
             fprintf(index_tmp, "%s %hhu %lld %s\n", new_entry.hash, new_entry.status, new_entry.mtime, new_entry.path);
-         
+
             fprintf(index_tmp, "%s %hhu %lld %s\n", entry.hash, entry.status, entry.mtime, entry.path);
         } else {
             fprintf(index_tmp, "%s %hhu %lld %s\n", entry.hash, entry.status, entry.mtime, entry.path);
@@ -120,7 +123,7 @@ void meow_add(char* file){
     }
 
     printf("%s\n", rel_path);
-    if(!inserted){
+    if (!inserted) {
         uint8_t hex_hash[41];
         create_blob(path, work_dir, &st, hex_hash);
         strcpy(entry.hash, hex_hash);
@@ -133,7 +136,7 @@ void meow_add(char* file){
 
     rewind(index_tmp);
     fprintf(index_tmp, "%u\n", new_entries_amt);
- 
+
     fclose(index_tmp);
     fclose(index);
 
@@ -141,21 +144,62 @@ void meow_add(char* file){
         perror("Err: rename failed");
         return;
     }
-
 }
 
-int main(int argc, char** argv){
-    switch (argc){
-        case 2:{
-            if (!strcmp(argv[1], "init")) meow_init();
-            if (!strcmp(argv[1], "dir_traverse")) write_project_dir();
-            break;
-        } 
-        case 3:{
-            if (!strcmp(argv[1], "add")) meow_add(argv[2]);
+void meow_commit(char* msg) {
+    if (!msg) {
+        printf("Сomment not found");
+        return;
+    }
+    char work_dir[PATH_MAX];
+    char path_to_index[PATH_MAX];
+    char path_to_indextmp[PATH_MAX];
+    char path[PATH_MAX];
+
+    if (find_work_dir(work_dir) == -1) {
+        fprintf(stderr, "Error: .meow/ not found\n");
+        return;
+    }
+    snprintf(path_to_index, PATH_MAX, "%s/index", work_dir);
+
+    FILE* index = fopen(path_to_index, "rb");
+    if (!index) {
+        fprintf(stderr, "Error: index not found\n");
+        return;
+    }
+    int entries_amt;
+    fscanf(index, "%d", &entries_amt);
+    indexEntry* entries = (indexEntry*)malloc(sizeof(indexEntry) * entries_amt);
+    puts("Index content:");
+    printf("MSG: %s\n", msg);
+    for (int i = 0; i < entries_amt; i++) {
+        fscanf(index, "%40s %d %lld %s", entries[i].hash, &entries[i].status, &entries[i].mtime, entries[i].path);
+    }
+    write_tree(entries, entries_amt);
+
+    free(entries);
+    fclose(index);
+}
+
+int main(int argc, char** argv) {
+    switch (argc) {
+        case 2: {
+            if (!strcmp(argv[1], "init"))
+                meow_init();
+            if (!strcmp(argv[1], "dir_traverse"))
+                write_project_dir();
+            if (!strcmp(argv[1], "commit"))
+                puts("Сomment not found");
             break;
         }
-        default:{
+        case 3: {
+            if (!strcmp(argv[1], "add"))
+                meow_add(argv[2]);
+            if (!strcmp(argv[1], "commit"))
+                meow_commit(argv[2]);
+            break;
+        }
+        default: {
             puts("Err: No arguments");
             break;
         }
--- src/utils.c
+++ src/utils.c
@@ -1,24 +1,28 @@
 #include "meow.h"
+#include <stdio.h>
 
-char is_path_absolute(char* path){
-    if (!path || path[0] == '\0') return 0;
-    if (path[0] == '/' || path[0] == '\\') return 1; // Unix
-    if (isalpha(path[0]) && path[1] == ':') return 1; // Windows
+char is_path_absolute(char* path) {
+    if (!path || path[0] == '\0')
+        return 0;
+    if (path[0] == '/' || path[0] == '\\')
+        return 1; // Unix
+    if (isalpha(path[0]) && path[1] == ':')
+        return 1; // Windows
     return 0;
 }
 
-void get_object_path(char *dest, const char *work_dir, uint8_t *hash) {
+void get_object_path(char* dest, const char* work_dir, uint8_t* hash) {
     sprintf(dest, "%s/objects/%02x/", work_dir, hash[0]);
     mkdir(dest, 0777);
-    
+
     char hex_hash[41];
-    for(int i = 0; i < 20; i++) {
-        sprintf(hex_hash + i*2, "%02x", hash[i]);
+    for (int i = 0; i < 20; i++) {
+        sprintf(hex_hash + i * 2, "%02x", hash[i]);
     }
     strcat(dest, hex_hash + 2);
 }
 
-int make_path_relative(const char *root, const char *input, char *output) {
+int make_path_relative(const char* root, const char* input, char* output) {
     char res[PATH_MAX];
 
     if (realpath(input, res) == NULL) {
@@ -29,7 +33,7 @@ int make_path_relative(const char *root, const char *input, char *output) {
         return -2;
     }
 
-    const char *relative_ptr = res + strlen(root);
+    const char* relative_ptr = res + strlen(root);
 
     if (*relative_ptr == '/') {
         relative_ptr++;
@@ -40,32 +44,32 @@ int make_path_relative(const char *root, const char *input, char *output) {
     return 0;
 }
 
-char find_work_dir(char* buffer){
+char find_work_dir(char* buffer) {
     char cwd[PATH_MAX];
     getcwd(cwd, PATH_MAX);
 
-    while(1){
+    while (1) {
         char tmp_path[PATH_MAX];
         snprintf(tmp_path, sizeof(tmp_path), "%s/.meow", cwd);
 
         struct stat st;
 
-        if(!stat(tmp_path, &st) && S_ISDIR(st.st_mode)){
+        if (!stat(tmp_path, &st) && S_ISDIR(st.st_mode)) {
             strcpy(buffer, tmp_path);
             return 0;
         }
 
-        if (!strcmp(cwd, "/")){
+        if (!strcmp(cwd, "/")) {
             break; // root
         }
 
         char* last_slash = strrchr(cwd, '/');
-        if(last_slash != NULL){
-            if (last_slash == cwd){
+        if (last_slash != NULL) {
+            if (last_slash == cwd) {
                 strcpy(cwd, "/"); // root
-            } else{
+            } else {
                 *last_slash = '\0';
-            } 
+            }
         } else {
             break;
         }
@@ -73,18 +77,17 @@ char find_work_dir(char* buffer){
     return -1;
 }
 
-
-void find_project_dir(char* buffer, char* work_dir){
+void find_project_dir(char* buffer, char* work_dir) {
     int len = strlen(work_dir);
     if (len > 6) {
         strncpy(buffer, work_dir, len - 6);
-        buffer[len-6] = '\0';
+        buffer[len - 6] = '\0';
     } else {
         strcpy(buffer, "/");
     }
 }
 
-char create_blob(char* path, char* work_dir, struct stat* st, char* hash){
+char create_blob(char* path, char* work_dir, struct stat* st, char* hash) {
     char work_obj_dir[PATH_MAX];
     char path_to_tempfile[PATH_MAX];
     char path_to_blob_dir[PATH_MAX];
@@ -99,9 +102,11 @@ char create_blob(char* path, char* work_dir, struct stat* st, char* hash){
     SHA1Init(&sha);
 
     FILE* f = fopen(path, "rb");
-    if (!f) assert("Cannot open file");
-    FILE* tempfile = fopen(path_to_tempfile, "w+b");
-    if (!tempfile) assert("Cannot create tempfile");
+    if (!f)
+        assert("Cannot open file");
+    FILE* tempfile = tmpfile();
+    if (!tempfile)
+        assert("Cannot create tempfile");
 
     char read_buffer[CHUNK];
 
@@ -114,13 +119,14 @@ char create_blob(char* path, char* work_dir, struct stat* st, char* hash){
     fwrite(header, sizeof(char), h_len, tempfile);
 
     size_t bytes_read;
-    while((bytes_read = fread(read_buffer, sizeof(uint8_t), CHUNK, f)) > 0){
+    while ((bytes_read = fread(read_buffer, sizeof(uint8_t), CHUNK, f)) > 0) {
         SHA1Update(&sha, (uint8_t*)read_buffer, bytes_read);
         fwrite(read_buffer, sizeof(uint8_t), bytes_read, tempfile);
-    } 
+    }
     SHA1Final(binary_hash, &sha);
 
-    for (int i = 0; i < 20; i++) sprintf(hash+(i*2), "%02x", binary_hash[i]);    
+    for (int i = 0; i < 20; i++)
+        sprintf(hash + (i * 2), "%02x", binary_hash[i]);
     hash[40] = '\0';
 
     char blob_dir_name[3];
@@ -130,12 +136,12 @@ char create_blob(char* path, char* work_dir, struct stat* st, char* hash){
     snprintf(path_to_blob_dir, PATH_MAX, "%s/%s", work_obj_dir, blob_dir_name);
     mkdir(path_to_blob_dir, 0777);
 
-    const char* blob_file_name = hash+2;
+    const char* blob_file_name = hash + 2;
     snprintf(path_to_blob, PATH_MAX, "%s/%s", path_to_blob_dir, blob_file_name);
 
-
     FILE* blob = fopen(path_to_blob, "wb");
-    if (!blob) assert("Cannot create blob");
+    if (!blob)
+        assert("Cannot create blob");
     rewind(tempfile); // return cursor
     def(tempfile, blob, Z_DEFAULT_COMPRESSION);
 
@@ -146,6 +152,19 @@ char create_blob(char* path, char* work_dir, struct stat* st, char* hash){
     remove(path_to_tempfile);
     fclose(blob);
     return 0;
+}
+
+void write_tree(indexEntry* entries, int entries_amt) {
+    FILE* tree_obj = tmpfile();
+    for (int i = 0; i < entries_amt; i++) {
+        char* something = strrchr(entries[i].path, '/');
+        // in root dir
+        if (!something) {
+            fprintf(tree_obj, "100644 blob %s\n", entries[i].hash, entries[i].path);
+        } else {
+            *something = '\0';
+        }
+    }
 }
 
 /* zpipe.c: example of proper use of zlib's inflate() and deflate()
@@ -164,14 +183,12 @@ char create_blob(char* path, char* work_dir, struct stat* st, char* hash){
  */
 
 #if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__)
-#  include <fcntl.h>
-#  include <io.h>
-#  define SET_BINARY_MODE(file) _setmode(_fileno(file), _O_BINARY)
+#include <fcntl.h>
+#include <io.h>
+#define SET_BINARY_MODE(file) _setmode(_fileno(file), _O_BINARY)
 #else
-#  define SET_BINARY_MODE(file)
+#define SET_BINARY_MODE(file)
 #endif
-
-#define CHUNK 16384
 
 /* Compress from file source to file dest until EOF on source.
    def() returns Z_OK on success, Z_MEM_ERROR if memory could not be
@@ -179,8 +196,7 @@ char create_blob(char* path, char* work_dir, struct stat* st, char* hash){
    level is supplied, Z_VERSION_ERROR if the version of zlib.h and the
    version of the library linked do not match, or Z_ERRNO if there is
    an error reading or writing the files. */
-int def(FILE *source, FILE *dest, int level)
-{
+int def(FILE* source, FILE* dest, int level) {
     int ret, flush;
     unsigned have;
     z_stream strm;
@@ -210,19 +226,19 @@ int def(FILE *source, FILE *dest, int level)
         do {
             strm.avail_out = CHUNK;
             strm.next_out = out;
-            ret = deflate(&strm, flush);    /* no bad return value */
-            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+            ret = deflate(&strm, flush);   /* no bad return value */
+            assert(ret != Z_STREAM_ERROR); /* state not clobbered */
             have = CHUNK - strm.avail_out;
             if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
                 (void)deflateEnd(&strm);
                 return Z_ERRNO;
             }
         } while (strm.avail_out == 0);
-        assert(strm.avail_in == 0);     /* all input will be used */
+        assert(strm.avail_in == 0); /* all input will be used */
 
         /* done when last data in file processed */
     } while (flush != Z_FINISH);
-    assert(ret == Z_STREAM_END);        /* stream will be complete */
+    assert(ret == Z_STREAM_END); /* stream will be complete */
 
     /* clean up and return */
     (void)deflateEnd(&strm);
@@ -235,7 +251,7 @@ int def(FILE *source, FILE *dest, int level)
    invalid or incomplete, Z_VERSION_ERROR if the version of zlib.h and
    the version of the library linked do not match, or Z_ERRNO if there
    is an error reading or writing the files. */
-int inf(FILE *source, FILE *dest){
+int inf(FILE* source, FILE* dest) {
     int ret;
     unsigned have;
     z_stream strm;
@@ -268,14 +284,14 @@ int inf(FILE *source, FILE *dest){
             strm.avail_out = CHUNK;
             strm.next_out = out;
             ret = inflate(&strm, Z_NO_FLUSH);
-            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+            assert(ret != Z_STREAM_ERROR); /* state not clobbered */
             switch (ret) {
-            case Z_NEED_DICT:
-                ret = Z_DATA_ERROR;     /* and fall through */
-            case Z_DATA_ERROR:
-            case Z_MEM_ERROR:
-                (void)inflateEnd(&strm);
-                return ret;
+                case Z_NEED_DICT:
+                    ret = Z_DATA_ERROR; /* and fall through */
+                case Z_DATA_ERROR:
+                case Z_MEM_ERROR:
+                    (void)inflateEnd(&strm);
+                    return ret;
             }
             have = CHUNK - strm.avail_out;
             if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
@@ -293,26 +309,25 @@ int inf(FILE *source, FILE *dest){
 }
 
 /* report a zlib or i/o error */
-void zerr(int ret)
-{
+void zerr(int ret) {
     fputs("zpipe: ", stderr);
     switch (ret) {
-    case Z_ERRNO:
-        if (ferror(stdin))
-            fputs("error reading stdin\n", stderr);
-        if (ferror(stdout))
-            fputs("error writing stdout\n", stderr);
-        break;
-    case Z_STREAM_ERROR:
-        fputs("invalid compression level\n", stderr);
-        break;
-    case Z_DATA_ERROR:
-        fputs("invalid or incomplete deflate data\n", stderr);
-        break;
-    case Z_MEM_ERROR:
-        fputs("out of memory\n", stderr);
-        break;
-    case Z_VERSION_ERROR:
-        fputs("zlib version mismatch!\n", stderr);
+        case Z_ERRNO:
+            if (ferror(stdin))
+                fputs("error reading stdin\n", stderr);
+            if (ferror(stdout))
+                fputs("error writing stdout\n", stderr);
+            break;
+        case Z_STREAM_ERROR:
+            fputs("invalid compression level\n", stderr);
+            break;
+        case Z_DATA_ERROR:
+            fputs("invalid or incomplete deflate data\n", stderr);
+            break;
+        case Z_MEM_ERROR:
+            fputs("out of memory\n", stderr);
+            break;
+        case Z_VERSION_ERROR:
+            fputs("zlib version mismatch!\n", stderr);
     }
 }
