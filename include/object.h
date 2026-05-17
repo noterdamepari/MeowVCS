774e6e6b094475a49507770c923c58a8275b19e2
noterdamepari <noterdamepari@icloud.com>
2026-05-17 12:36:48 +0700

    another code improves

--- include/object.h
+++ include/object.h
@@ -4,7 +4,8 @@
 #include <sys/stat.h>
 typedef enum {
     BLOB = 0,
-    TREE
+    TREE,
+    COMMIT
 } object_type;
 
 void hash_and_create_obj(object_type type, struct stat* st, FILE* f, char* ohash);
