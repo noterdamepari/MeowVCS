95c92dc899658b20d6cfa02d306c32c1f5f466ad
noterdamepari <noterdamepari@icloud.com>
2026-05-17 13:29:45 +0700

    config added

--- src/cmds/meow_init.c
+++ src/cmds/meow_init.c
@@ -1,4 +1,5 @@
 #include "meow.h"
+#include <stdio.h>
 
 char meow_init() {
     char def_path[PATH_MAX];
@@ -9,16 +10,28 @@ char meow_init() {
     strcat(def_path, default_dir);
     mkdir(def_path, 0777);
 
-    strcpy(path, def_path);
-    strcat(path, meow_index);
+    snprintf(path, PATH_MAX, "%s/index", def_path);
     FILE* index = fopen(path, "wb");
     if (!index)
         perror("Cannot create index");
     fprintf(index, "0");
     fclose(index);
 
-    strcpy(path, def_path);
-    strcat(path, objects_dir);
+    snprintf(path, PATH_MAX, "%s/config", def_path);
+    FILE* cfg = fopen(path, "wb");
+    if (!cfg)
+        perror("Cannot create config file");
+    char buffer[255];
+    printf("Your username: ");
+    scanf("%s", buffer);
+    fprintf(cfg, "%s\n", buffer);
+    printf("Your email: ");
+    scanf("%s", buffer);
+    fprintf(cfg, "%s\n", buffer);
+    puts("Project initialized");
+    fclose(cfg);
+
+    snprintf(path, PATH_MAX, "%s%s", def_path, objects_dir);
     mkdir(path, 0777);
     return 0;
 }
